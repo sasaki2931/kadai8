a5c666ea0c59287a72fe17baf0c9ac0ed281f3db
syunsuke sasaki <sasaki2931@gmail.com>
2022-12-29 15:27:33 +0900

    アソシエーション定義

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_29_061540) do
+ActiveRecord::Schema.define(version: 2022_12_29_062126) do
 
   create_table "properties", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 2022_12_29_061540) do
     t.string "distance"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "property_id"
+    t.index ["property_id"], name: "index_stations_on_property_id"
   end
 
+  add_foreign_key "stations", "properties"
 end
